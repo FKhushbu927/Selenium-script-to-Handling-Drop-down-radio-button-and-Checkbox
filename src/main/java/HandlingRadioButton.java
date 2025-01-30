52a8fd6a7a5bbc54f4b63f15f63480046909f885
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-01-30 19:30:31 +0600

    selenium-script-to-check-Radio-button-updated

--- src/main/java/HandlingRadioButton.java
+++ src/main/java/HandlingRadioButton.java
@@ -1,6 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 
 public class HandlingRadioButton {
 
@@ -9,6 +13,19 @@ public class HandlingRadioButton {
         WebDriver driver = new FirefoxDriver();
 
         driver.get("https://demo.guru99.com/test/radio.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.findElement(By.id("vfb-7-1")).isDisplayed();
+
+        WebElement element =driver.findElement(By.id("vfb-7-1"));
+
+        if(!(element.isSelected())){
+            driver.findElement(By.id("vfb-7-1")).click();
+        }
+
+
+
+
 
     }
 }
