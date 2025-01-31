aa54626d845b1c8c16fa203e2a352fd250b7b5bb
FKhushbu927 <jannateekhushbu927@gmail.com>
2025-01-31 15:08:48 +0600

    selenium-script-Handling-DropDownList-updated

--- src/main/java/HandlingDropDownList.java
+++ src/main/java/HandlingDropDownList.java
@@ -1,6 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
 public class HandlingDropDownList {
 
     public static void main(String[] args) {
+
+        WebDriverManager.firefoxdriver().clearDriverCache().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://demoqa.com/select-menu");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,300)", "");
+
+        WebElement element = driver.findElement(By.id("oldSelectMenu"));
+        Select select = new Select(element);
+       // select.selectByIndex(8);
+        //select.selectByValue("10");
+       // select.selectByValue("10");
+        select.selectByVisibleText("Magenta");
 
 
     }
